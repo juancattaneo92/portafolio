f5f9cac8d148cfa61d6ff98774e54dd4df76bd61
Juan <juancattaneo92@gmail.com>
2021-04-19 13:16:50 -0700

    added google analytics

---
+++ assets/js/ga-events.js
@@ -0,0 +1,10 @@
+// function handleGAEvent(action, event_category, event_label){
+//   return((_) => {
+//     console.log('analytics')
+//     gtag('event', action, { event_category, event_label}
+//     );
+//   });
+
+// }
+
+// document.querySelector('button wide')
